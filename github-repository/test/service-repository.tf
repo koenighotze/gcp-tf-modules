0846b9cd15d498745c422bd9118f12074be7686f
David Schmitz <davidschmitz@posteo.de>
2022-03-11 10:45:18 +0100

    Docu update

--- github-repository/test/service-repository.tf
+++ github-repository/test/service-repository.tf
@@ -5,10 +5,10 @@ module "service_repository" {
   codacy_api_token                = random_uuid.id.result
   docker_registry_username        = random_uuid.id.result
   docker_registry_token           = random_uuid.id.result
-  project_id                  = var.project_id
+  project_id                      = var.project_id
   workload_identity_provider_name = random_uuid.id.result
   workload_identity_pool_id       = random_uuid.id.result
-  deployer_service_account_email     = var.sa_email
+  deployer_service_account_email  = var.sa_email
   github_admin_token              = random_uuid.id.result
   github_non_admin_token          = random_uuid.id.result
   terraform_state_bucket          = random_uuid.id.result
