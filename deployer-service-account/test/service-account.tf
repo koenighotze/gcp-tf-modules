b4df46d5ab42e0e281f87f7b34ec6ef1bb7df860
David Schmitz <davidschmitz@posteo.de>
2022-03-12 16:04:21 +0100

    Working on service setup

--- deployer-service-account/test/service-account.tf
+++ deployer-service-account/test/service-account.tf
@@ -1,6 +1,6 @@
 module "service_repository_deployer_sa" {
   source = "../"
 
-  name           = "test-${random_uuid.random_id.result}"
+  name       = "test-${random_uuid.random_id.result}"
   project_id = var.project_id
 }
