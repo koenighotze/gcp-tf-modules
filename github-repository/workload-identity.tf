28fab0250aa23464479e8e2104392a0c343da101
David Schmitz <davidschmitz@posteo.de>
2022-03-13 15:04:29 +0100

    SA needs full repo name for workload identity to work

--- github-repository/workload-identity.tf
+++ github-repository/workload-identity.tf
@@ -4,6 +4,6 @@ resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
   role               = "roles/iam.workloadIdentityUser"
 
   members = [
-    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/${github_repository.repository.name}",
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/${github_repository.repository.full_name}",
   ]
 }
