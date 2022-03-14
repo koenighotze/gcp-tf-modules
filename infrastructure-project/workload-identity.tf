cad798992847adb7b7adf75ed0954f48e8531017
David Schmitz <davidschmitz@posteo.de>
2022-03-14 08:20:45 +0100

    WI needs SA id

--- infrastructure-project/workload-identity.tf
+++ infrastructure-project/workload-identity.tf
@@ -12,7 +12,7 @@ resource "github_actions_secret" "workload_identity_pool_id" {
 
 # Allow repository to use deployer service account
 resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
-  service_account_id = var.service_account_email
+  service_account_id = data.google_service_account.sa.id
   role               = "roles/iam.workloadIdentityUser"
 
   members = [
