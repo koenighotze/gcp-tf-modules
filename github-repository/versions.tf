f00a5f1d1b457993f1dfabe4fb146062c1fcac64
David Schmitz <davidschmitz@posteo.de>
2022-03-18 11:46:59 +0100

    Cleanup

--- github-repository/versions.tf
+++ github-repository/versions.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.1.6"
   required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.52.0"
+    }
+
     github = {
       source  = "integrations/github"
       version = ">= 4.0"
---
+++ github-repository/workload-identity.tf
@@ -0,0 +1,9 @@
+# Allow repository to use deployer service account
+resource "google_service_account_iam_binding" "workload_identity_sa_binding" {
+  service_account_id = data.google_service_account.deployer_sa.id
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "principalSet://iam.googleapis.com/${var.workload_identity_pool_id}/attribute.repository/${github_repository.repository.full_name}",
+  ]
+}
