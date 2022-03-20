c4ec1c30b5981436a6136edf49c6431a1ac33cbc
David Schmitz <davidschmitz@posteo.de>
2022-03-20 16:25:48 +0100

    Unique name for repo

--- infrastructure-project/outputs.tf
+++ infrastructure-project/outputs.tf
@@ -5,3 +5,7 @@ output "github_repository_full_name" {
 output "github_repository_id" {
   value = github_repository.infrastructure_repository.id
 }
+
+output "state_bucket_url" {
+  value = google_storage_bucket.state_bucket.url
+}
