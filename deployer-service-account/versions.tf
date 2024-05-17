ac7fd24ac70db5cbb5ac45ecc7e68ac96200175f
David Schmitz <david.schmitz.privat@gmail.com>
2024-05-17 12:45:12 +0200

    Cleanup

--- deployer-service-account/versions.tf
+++ deployer-service-account/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.1.6"
+  required_version = ">= 1.8"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.52.0"
+      version = "~> 5.0"
     }
   }
 }
