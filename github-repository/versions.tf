25a3875246f19faf743391b368fbc4b422b348bb
David Schmitz <david.schmitz.privat@gmail.com>
2024-05-24 13:47:07 +0200

    WIP

--- github-repository/versions.tf
+++ github-repository/versions.tf
@@ -1,26 +1,14 @@
 terraform {
-  required_version = ">= 1.8"
+  required_version = ">= 1.1.6"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 5.0"
+      version = ">= 3.52.0"
     }
 
     github = {
-      source = "hashicorp/github"
-      # version = "~> 6.0"
-      # version = "~> 5.0"
-      version = "~> 5.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6.0"
+      source  = "integrations/github"
+      version = ">= 4.0"
     }
   }
 }
